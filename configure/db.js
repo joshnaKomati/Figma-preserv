9f7dd016f2530edea017e782bbdda4dc214b67ec
Sravan komati <sravankomati12@gmail.com>
2022-11-19 17:50:29 +0530

    kjnj

--- configure/db.js
+++ configure/db.js
@@ -1,4 +1,11 @@
 const mongoose=require("mongoose")
-mongoose.connect('mongodb://localhost:27017/proserve',()=>{
-    console.log("db is connected");
-})
+require("dotenv").config();
+// mongoose.connect('mongodb+srv://joshna123:Joshna@cluster0.ke8bcxw.mongodb.net/proserve',(err)=>{
+    mongoose.connect(process.env.dbConnection,(err)=>{
+        if(err){
+            console.log(err.message);
+        }else{
+            console.log("db is connected");
+        }
+    })
+   
